7787bcd2e588c0383e971a34a285ef35eda6400c
Tejas Sanap <sanap.tejas@gmail.com>
2021-09-17 20:22:25 +0530

    Make `eventloop` module private and add `core` module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 mod network;
 
 use network::client;
-use network::eventloop;
 
 // Ideally, speaking this fiel should only interact with the Client API defined in
 // network/client.rs
--- src/network.rs
+++ src/network.rs
@@ -1,2 +1,7 @@
+// This is the event loopm the client API will interact with.
+mod eventloop;
+// The core module defines the `ComposedBehaviour` and `ComposedEvent`
+mod core;
+
+// Exposed API.
 pub(crate) mod client;
-pub(crate) mod eventloop;
